d666247e293f0ad74078bd48e6086672342a05f5
Cristobal <cris_99@hotmail.es>
2024-10-17 16:05:13 -0700

    firebase test

---
+++ src/js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB6xYzpm1j1he5A4c-AbvKEJgcwIAgnLow",
+    authDomain: "testmidexam.firebaseapp.com",
+    projectId: "testmidexam",
+    storageBucket: "testmidexam.appspot.com",
+    messagingSenderId: "54942996376",
+    appId: "1:54942996376:web:6c73bdad52f99d04d07ded"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
